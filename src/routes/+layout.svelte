abfa1c954401a0cfa3334f684e2ceccf752e2cbe
lbabino <lucasbabino@gmail.com>
2023-06-15 19:26:20 -0300

    added "sitio en construccion" warning

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,21 @@
 <script>
 	import '../app.postcss';
 </script>
-<nav class="p-2 bg-slate-500 text-white">
+<nav class="p-2 bg-slate-500 text-white items-center">
     <a href="/">NuestroServer</a>
+    <div class="p-2 bg-amber-200 rounded-xl flex flex-row items-center">
+        <div>
+            <p class="text-blue-950 text-[0.85em]"><i>¡Sitio en contrucción!</i></p>
+            <p class="text-blue-950 text-[0.65em]"><i>Disculpe las molestias</i></p>
+        </div>
+        <p class="text-2xl">👷‍♂️</p>
+    </div>
     <a href="/getting-started">Intro rápida</a>
 </nav>
-<div class="container">
+<div class="container max-w-prose">
     <slot></slot>
 </div>
 
 <style>
-    .container{
-        max-width: 70ch;
-        margin: 0 auto;
-    }
 </style>
+
