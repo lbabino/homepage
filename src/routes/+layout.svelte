e7b5deda2f4afa75078929481ab00028ad194f28
lbabino <lucasbabino@gmail.com>
2023-06-15 19:31:09 -0300

    experimental shadows

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,15 @@
 	import '../app.postcss';
 </script>
 <nav class="p-2 bg-slate-500 text-white items-center">
-    <a href="/">NuestroServer</a>
-    <div class="p-2 bg-amber-200 rounded-xl flex flex-row items-center">
+    <a class="ml-2 hover:shadow-sm hover:shadow-black transition-shadow hover:duration-75" href="/">NuestroServer</a>
+    <div class="p-2 bg-amber-200 rounded-xl flex flex-row items-center shadow-sm shadow-black">
         <div>
             <p class="text-blue-950 text-[0.85em]"><i>¡Sitio en contrucción!</i></p>
             <p class="text-blue-950 text-[0.65em]"><i>Disculpe las molestias</i></p>
         </div>
-        <p class="text-2xl">👷‍♂️</p>
+        <p class="text-2xl ">👷‍♂️</p>
     </div>
-    <a href="/getting-started">Intro rápida</a>
+    <a class="mr-2 hover:shadow-sm hover:shadow-black transition-shadow hover:duration-75" href="/getting-started">Intro rápida</a>
 </nav>
 <div class="container max-w-prose">
     <slot></slot>
